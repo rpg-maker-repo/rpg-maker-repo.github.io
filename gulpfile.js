44fc4ff7c8a8c5772416c2a222b29408d91281d7
Filipe Oliveira <contato@fmoliveira.com.br>
2015-11-28 18:45:03 -0200

    Custom Bootstrap without Glyphicons.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,28 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	minify = require('gulp-minify-css'),
+	rename = require('gulp-rename');
 
 const bower = './bower_components';
 const bootstrap = `${bower}/bootstrap-sass/assets`;
+const src = './src';
 const dist = './dist';
 
+// custom bootstrap build
 gulp.task('css', () => {
-	// custom bootstrap build
+	gulp.src(`${src}/sass/*.{sass,scss}`)
+		.pipe(sass({
+			includePaths: `${bootstrap}/stylesheets/`
+		}))
+		.pipe(gulp.dest(`${dist}/css/`))
+		.pipe(sourcemaps.init())
+		.pipe(minify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(`${dist}/css/`));
 });
 
 gulp.task('fonts', () => {
@@ -14,11 +31,19 @@ gulp.task('fonts', () => {
 
 gulp.task('js', () => {
 	gulp.src(`${bootstrap}/javascripts/bootstrap.{js,min.js}`)
-		.pipe(gulp.dest(`${dist}/js`));
+		.pipe(gulp.dest(`${dist}/js/`));
 });
 
-gulp.task('default', [
+gulp.task('build', [
 	'css',
 	'fonts',
 	'js'
+]);
+
+gulp.task('develop', [
+	'build'
+]);
+
+gulp.task('default', [
+	'build'
 ]);
