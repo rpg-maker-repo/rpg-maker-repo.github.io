0057c4de6da11188604527ed484cb643feadfdc2
Filipe Oliveira <contato@fmoliveira.com.br>
2015-11-29 00:30:53 -0200

    Enforcing CORS.

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,14 @@ angular.module('AssetsApp', [])
 .controller('HomeCtrl', function ($http) {
 	var vm = this;
 
-	$http.get('https://deusprogrammer.no-ip.org/rmmv-api/v1/plugin')
+	//$http.get('https://deusprogrammer.no-ip.org/rmmv-api/v1/plugin')
+	$http({method: 'GET', url: 'https://deusprogrammer.no-ip.org/rmmv-api/v1/plugin', 
+            headers:{
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, X-Requested-With',
+                'X-Random-Shit':'123123123'
+            }})
 		.then(function (data) {
 			vm.list = data;
 		}, function (err) {
